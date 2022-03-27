147788f2f2c3d4c6f02da3c740b46646a7e5a222
David Daniel Mateiu <daniel.mateiu@redbull.com>
2022-03-27 09:17:44 +0200

    test: remove obsolete await

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -2,8 +2,8 @@ import { start } from '../index';
 
 // TODO remove on implementation
 describe('index.ts', () => {
-    it('returns `Hello World!`', async () => {
-        const result = await start();
+    it('returns `Hello World!`', () => {
+        const result = start();
 
         expect(result).toStrictEqual('Hello World!');
     });
